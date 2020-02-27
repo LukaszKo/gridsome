eea7e37c92bcfb627f457e16d1b8e34f6ba301be
lkochajewski@pgs-soft.com <lkochajewski@pgs-soft.com>
2020-02-27 13:22:51 +0100

    create new template for posts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,11 @@
   <Layout>
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <g-image
+      alt="Example image"
+      src="~/favicon.png"
+      width="135"
+    />
 
     <h1>Hello, world!</h1>
 
@@ -11,23 +15,56 @@
     </p>
 
     <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a
+        href="https://gridsome.org/docs/"
+        target="_blank"
+        rel="noopener"
+      >Gridsome Docs</a>
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener"
+      >GitHub</a>
     </p>
+
+    <ul>
+      <li
+        v-for="post in $page.posts.edges"
+        :key="post.id"
+      >
+        <g-link :to="post.node.path">
+          {{ post.node.title }}
+        </g-link>
+      </li>
+    </ul>
+    <!-- <pre>{{$page.posts.edges}}</pre> -->
 
   </Layout>
 </template>
 
+<page-query>
+query Posts {
+  posts: allPost {
+    edges {
+      node {
+        id,
+        path,
+        title
+      }
+    }
+  }
+}
+</page-query>
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   }
-}
+};
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
-}
+  .home-links a {
+    margin-right: 1rem;
+  }
 </style>
