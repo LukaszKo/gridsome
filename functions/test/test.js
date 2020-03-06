ce9dec876580ef1c82f2ebc86983f8c0bd802194
lkochajewski@pgs-soft.com <lkochajewski@pgs-soft.com>
2020-03-06 11:49:21 +0100

    add connection to database and update test function

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,19 +1,24 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const faunadb = require("faunadb");
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+});
+
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || "World";
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept"
-      }
-    };
+    client.query(
+      q.Get(q.Ref(q.Collection("products")).then(res => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(res),
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers":
+              "Origin, X-Requested-With, Content-Type, Accept"
+          }
+        };
+      })
+    );
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
   }
