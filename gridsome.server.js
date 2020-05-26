349da54a68046a5b1e1a4b077db7c7e32b0f567a
Lukasz_Kochajewski <lkochajewski@pgs-soft.com>
2020-05-26 14:26:18 +0200

    update

--- gridsome.server.js
+++ gridsome.server.js
@@ -10,34 +10,34 @@ module.exports = function (api) {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
   })
 
-  api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`
-      {
-        allPost {
-          edges {
-            node {
-              id,
-              path,
-              title,
-              excerpt,
-              image
-            }
-          }
-        }
-      }
-    `)
+  // api.createPages(async ({ graphql, createPage }) => {
+  //   const { data } = await graphql(`
+  //     {
+  //       allPost {
+  //         edges {
+  //           node {
+  //             id,
+  //             path,
+  //             title,
+  //             excerpt,
+  //             image
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `)
 
-    data.allPost.edges.forEach(({ node }) => {
-      createPage({
-        path: node.path,
-        component: './src/templates/Post.vue',
-        context: {
-          id: node.id,
-          title: node.title,
-          image: node.image,
-          excerpt: node.excerpt
-        }
-      })
-    })
-  })
+  //   data.allPost.edges.forEach(({ node }) => {
+  //     createPage({
+  //       path: `/new${node.path}`,
+  //       component: './src/templates/Post.vue',
+  //       context: {
+  //         id: node.id,
+  //         title: node.title,
+  //         image: node.image,
+  //         excerpt: node.excerpt
+  //       }
+  //     })
+  //   })
+  // })
 }
